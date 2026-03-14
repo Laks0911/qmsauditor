459726d2261280eb5e0998d087bf0a71cb2a5a54
Laks0911 <maxvisionconstruction@gmail.com>
2026-03-14 21:43:42 +0000

    S1-02: Custom User model with Admin/Auditor/Viewer roles

--- qmsauditor/settings.py
+++ qmsauditor/settings.py
@@ -51,7 +51,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "audits",
+    "users",
 ]
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
